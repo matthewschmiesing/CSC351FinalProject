fc94b93f74bf29664ac40a7d96ef1d7679190a70
matthewschmiesing <schmiesingmatthew@gmail.com>
2025-04-02 14:06:04 -0400

    added like toggle button to all the cards

--- jsJsonRetrieval/SnacksRecipes.js
+++ jsJsonRetrieval/SnacksRecipes.js
@@ -18,21 +18,49 @@ function displayRecipes(recipes, containerId) {
         card.classList.add("recipeCardBox");
 
         card.innerHTML = `
-            <div class="leftB">
-                <h4>${recipe.name}</h4>
-                <ul>
-                    <li><strong>Category:</strong> ${recipe.category}</li>
-                    <li><strong>Season:</strong> ${recipe.season}</li>
-                    <li><strong>Cuisine:</strong> ${recipe.cuisine}</li>
-                    <li><strong>Prep Time:</strong> ${recipe.prep_time}</li>
-                    <li><strong>Difficulty:</strong> ${recipe.difficulty}</li>
-                </ul>
+            <div class="column">
+                <div class="cardRow">
+                    <div class="leftC"></div>
+                    <div class="like-container rightC">
+                        <button class="like-btn" data-recipe-id="${recipe.id}">&#x2661</button>
+                    </div>
+                </div>
+                <div class="cardRow">
+                    <div class="leftB">
+                        <h4 class="clickable" data-recipe-id="${recipe.id}">${recipe.name}</h4>
+                        <ul>
+                            <li class="clickable" data-recipe-id="${recipe.id}"><strong>Category:</strong> ${recipe.category}</li>
+                            <li class="clickable" data-recipe-id="${recipe.id}"><strong>Season:</strong> ${recipe.season}</li>
+                            <li class="clickable" data-recipe-id="${recipe.id}"><strong>Cuisine:</strong> ${recipe.cuisine}</li>
+                            <li class="clickable" data-recipe-id="${recipe.id}"><strong>Prep Time:</strong> ${recipe.prep_time}</li>
+                            <li class="clickable" data-recipe-id="${recipe.id}"><strong>Difficulty:</strong> ${recipe.difficulty}</li>
+                        </ul>
+                    </div>
+                    <div class="rightB imageWrapper">
+                        <img class="clickable" src="recipeImage/${recipe.images[0]}" alt="Image of ${recipe.name}" data-recipe-id="${recipe.id}">
+                    </div>
+                </div>
+                
             </div>
-            <div class="rightB imageWrapper">
-                <img src="recipeImage/${recipe.images[0]}" alt="Image of ${recipe.name}">
-            </div>
+            
         `;
 
         container.appendChild(card);
     });
+
+    //event listener like button
+    container.addEventListener("click", (event) => {
+        if (event.target.classList.contains("clickable")) {
+            const recipeId = event.target.dataset.recipeId;
+            window.location.href = `recipeDetails.html?id=${recipeId}`;
+        }
+    });
+
+    // Like button
+    container.addEventListener("click", (event) => {
+        if (event.target.classList.contains("like-btn")) {
+            event.target.classList.toggle("liked");
+        }
+    });
 }
+
